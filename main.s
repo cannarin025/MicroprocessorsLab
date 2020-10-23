9865470b8fb7b921ae7b7b9fb8c140664403c0ee
canna <cannarin025@gmail.com>
2020-10-23 12:20:02 +0100

    debugging read/write subroutines

--- main.s
+++ main.s
@@ -13,45 +13,45 @@ start:
 	call	read_data
 	goto	$
 
-	; data on portE
+	; data on portD
 	; CP1 portE 1
 	; OE1 poetE 0
 	
 write_data:
 	movlw	0x00
+	movwf	TRISD, A ;sets PORTD to output
 	movwf	TRISE, A ;sets PORTE to output
-	movwf	TRISB, A ;sets PORTD to output
 	movlw	0x03	;keeps CP1 high and OE1 high. Last 4 bits 0001
-	movwf	PORTD, A 
+	movwf	PORTE, A 
 	
-	movlw   0x69    ;data to be outputted
+	movlw   0x30    ;data to be outputted
 	movwf   0x01, A
-	movff   0x01, LATE
+	movff   0x01, PORTD
 	call    write_loop
 	return	0
     
 write_loop:
 	movlw	0x01	;drop CP1 keeping OE1 high last 4 bits: 0001
-	movwf	PORTD, A
+	movwf	PORTE, A
 	movwf	0x03	;raise CP1 keeping OE1 high. last 4 bits: 0011
-	movwf	PORTD, A
+	movwf	PORTE, A
 	movlw	0x01
-	movwf	TRISE, A ;sets trisE to 1 (disables outputs?)
+	movwf	TRISD, A ;sets trisD to 1 (disables outputs?)
 	return	0
 
 read_data:
 	movlw	0x03 ;keep OE1 and CP1 high. last 4 bits: 0011
-	movwf	PORTD, A
-	movwf	0x01 ;disabling outputs of portE
-	movwf	TRISE, A
+	movwf	PORTE, A
+	movwf	0x01 ;disabling outputs of portD
+	movwf	TRISD, A
 	call	read_loop
 	return	0
 
 read_loop:
 	movlw	0x02 ;drop OE1, keeping CP1 high. Last 4 bits 0010
-	movwf	PORTD, A
+	movwf	PORTE, A
 	movlw	0x03 ;raise OE1, keeping CP1 high. Last 4 bits: 0011
-	movwf	PORTD, A
+	movwf	PORTE, A
 	
-	movff	PORTE, 0x02 ;stores read data in register 0x02
+	movff	PORTD, PORTJ ;stores read data on portJ
 	return	0

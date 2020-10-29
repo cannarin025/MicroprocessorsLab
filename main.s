ba261fda2ccf4fc6401fb88d89497dc8d6a4bd06
canna <cannarin025@gmail.com>
2020-10-29 11:40:07 +0000

    added 1s delay

--- main.s
+++ main.s
@@ -9,11 +9,12 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
+	call	SPI_MasterInit
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
-	call	delay_setup
+	call	delay1s_setup
 	incf 	0x06, W, A
 	movff 	0x06, PORTC
 test:
@@ -49,17 +50,56 @@ alternete_delay_start:
     movlw   0xFF    ;start value 2
     movwf   0x22, A
     movlw   0xFF
-    movwf   0x23
+    movwf   0x23, A
     call    alternate_delay
     return 0
    
 alternate_delay:
-    decfsz  0x21
+    decfsz  0x21, A
     bra	    alternate_delay
-    decfsz  0x22
+    decfsz  0x22, A
     bra	    alternate_delay
-    decfsz  0x23
+    decfsz  0x23, A
     bra	    alternate_delay
     return 0
-	
+    
+delay1s_setup: 
+    movlw 0xfd    ; set W = 256 (timer)
+    movwf 0x06, A    ; set timer f06 = W (=256)
+    movlw 0x2b
+    movwf 0x07, A    ; set timer f07 = W (=256)
+    movlw 0x52    ; set W = 80
+    movwf 0x08, A    ; set timer f08 = W (=80)
+    call delay1s    ; run the delay
+    return
+
+delay1s:
+    decfsz 0x06, A    ; decrement f02 and skip next if f02 = 0
+    bra delay1s
+    decfsz 0x07, A    ; decrement f02 and skip next if f02 = 0
+    bra delay1s
+    decfsz 0x08, A    ; decrement f02 and skip next if f02 = 0
+    bra delay1s
+    return
+
+
+SPI_MasterInit:	    ;set clock edge to negative
+    bcf	    CKE	    ;CKE bit in SSP2STAT
+		    ;MSSP enable; CKP=1; SPI master, click = Fosc/64 (1MHz)
+    movlw   (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+    movwf   SSP2CON1, A
+		    ;SDO2 output; SCK2 outpup
+    bcf	    TRISD, PORTD_SDO2_POSN, A	;SDO2 output
+    bcf	    TRISD, PORTD_SCK2_POSN, A	;SCK2 output
+    return
+    
+SPI_MasterTransmit: ;Start transmission of data (held in W)
+    movwf   SSP2BUF, A	;write data to output buffer
+ 
+Wait_transmit:	    ;wait for transmission to complete
+    btfss   SSP2IF  ;check interrupt flag to see if data has been sent
+    bra	    Wait_transmit
+    bcf	    SSP2IF  ;clear interrupt flag
+    return
+
     
